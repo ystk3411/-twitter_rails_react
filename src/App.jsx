cd1bbd953db95b68c4601912927ccfbe9649a473
ystk3411 <yoshi.hama0701@gmail.comgit push -u origin main>
2024-10-31 01:06:49 +0900

    ログイン画面を実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useState,useEffect } from 'react'
 import './App.css'
 import New from  './container/registration/New.jsx'
+import Sign_in from  './container/session/Sign_in.jsx'
 import Index from './Index.jsx'
-import axios from 'axios'
+import Cookies from 'js-cookie';
 import {
   BrowserRouter,
   Route,
@@ -11,13 +12,16 @@ import {
 
 
 function App() {
-
+  console.log(Cookies.get("access-token"))
+  const isLogIn = Cookies.get("access-token") ? true : false
+  console.log(isLogIn)
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />}/>
+          <Route path="/" element={isLogIn ? <Index /> : <Sign_in />}/>
           <Route path="registration/new" element={<New />}/>
+          <Route path="session/sign_in" element={<Sign_in />}/>
         </Routes>
       </BrowserRouter>
     </>
