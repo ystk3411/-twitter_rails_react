0ff888092a600c4892d349b46ab0f8bf175f34e1
ystk3411 <yoshi.hama0701@gmail.comgit push -u origin main>
2024-12-29 22:56:18 +0900

    ツイート削除機能を実装

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,5 +1,7 @@
 import { useState, useRef, useEffect } from 'react'
 import './App.css'
+import { NavLink } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
 import Header from './layouts/Header.jsx'
 import Sidebar from './layouts/Sidebar.jsx'
 import Tweets from './layouts/Tweets.jsx'
@@ -8,6 +10,8 @@ import Form from 'react-bootstrap/Form';
 import Pagination from 'react-bootstrap/Pagination';
 import instance from './axiosUtil.jsx'
 import { PiImageSquareFill } from "react-icons/pi";
+import alt from './assets/twitter_logo.jpg'
+import Cookies from 'js-cookie';
 
 function Index() {
   const [post, setPost] = useState({content:""})
@@ -15,8 +19,10 @@ function Index() {
   const [image, setImage] = useState()
   const [selectPage, setSelectPage] = useState(1)
   const [totalPage, setTotalPage] = useState(1)
+  const [user_image, setUserImage] = useState(null)
   const inputRef = useRef(null);
   const formData = new FormData()
+  const userId = Cookies.get("id");
 
   let items = Array.from({ length: totalPage }, (_, index) => {
     const number = index + 1
@@ -55,6 +61,7 @@ function Index() {
       const num = Math.floor(arrayPost / 10)
       setPosts(response.data.tweets)
       setTotalPage(num)
+      setUserImage(response.data.user_image)
     } catch(error) {
       console.log(error)
     }
@@ -88,30 +95,43 @@ function Index() {
       <Header />
       <div  className='container'>
         <Sidebar />
-        <Form className='postForm'>
-          <Form.Group className="mb-3" controlId="formGridAddress3" >
-            <Form.Control value={post.content} name="tweet" onChange={onChange} placeholder="今どうしてる？" />
-          </Form.Group>
-          <div>
-            <Button component="label" variant="outlined"  onClick={handleClick}>
-              <h4>
-                <PiImageSquareFill />
-              </h4>
-              <Form.Control type='file'  name="tweet" ref={inputRef} onChange={onChangeImage} hidden/>
-            </Button>
-          <Button className='postButton' variant="primary"  onClick={() => submitData()}>ポストする</Button>
-          </div>
-          <div className='tweets'>
-            <div>
-              {posts.map((tweet) => (
-                <>
-                  <Tweets tweet={tweet.tweet} user={tweet.user}/>
-                </>
-              ))}
+        <div className='w-50'>
+          <Form className='postForm'>
+            <div className='d-flex border-bottom p-2 gap-2'>
+              <Nav as="div">
+                <Nav.Link href={`user/${userId}`}>
+                {user_image ? <img src={user_image} className='rounded-circle border border-2 border-dark' width={40}/> : <img src={alt} className='rounded-circle border border-2 border-dark' width={40}/>}
+                </Nav.Link>
+              </Nav>
+              <div className='w-100'>
+                <Form.Group className="mb-3" controlId="formGridAddress3" >
+                  <Form.Control value={post.content} name="tweet" onChange={onChange} placeholder="今どうしてる？" />
+                </Form.Group>
+                <div className='d-flex justify-content-between'>
+                  <Button component="label" variant="outlined"  onClick={handleClick}>
+                    <h4>
+                      <PiImageSquareFill />
+                    </h4>
+                    <Form.Control type='file'  name="tweet" ref={inputRef} onChange={onChangeImage} hidden/>
+                  </Button>
+                  <Button className='postButton' variant="primary"  onClick={() => submitData()}>ポストする</Button>
+                </div>
+              </div>
+
             </div>
-            <Pagination>{items}</Pagination>
-          </div>
-        </Form>
+            
+            <div className='tweets'>
+              <div>
+                {posts.map((tweet) => (
+                  <>
+                    <Tweets tweet={tweet.tweet} user={tweet.user} image_user={tweet.image}/>
+                  </>
+                ))}
+              </div>
+              <Pagination className='justify-content-center'>{items}</Pagination>
+            </div>
+          </Form>
+        </div>
       </div>
     </>
   )
--- src/layouts/Tweets.jsx
+++ src/layouts/Tweets.jsx
@@ -1,36 +1,54 @@
-import Nav from 'react-bootstrap/Nav';
 import { NavLink } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
+import Dropdown from 'react-bootstrap/Dropdown';
 import { IoChatbubbleOutline } from "react-icons/io5";
 import { CiRepeat } from "react-icons/ci";
 import { CiHeart } from "react-icons/ci";
 import { IoStatsChart } from "react-icons/io5";
+import { HiDotsHorizontal } from "react-icons/hi";
+import { FaRegTrashAlt } from "react-icons/fa";
 import alt from '../assets/twitter_logo.jpg'
 import Cookies from 'js-cookie';
+import instance from '../axiosUtil.jsx'
 
 function Tweets(tweet) {
+  const userId = Cookies.get("id");
+  const onClickDelete = async(id) => {
+    const response = await instance.delete(`/tweets/${id}`, id);
+    window.location.reload();
+  }
   return (
     <div className='d-flex border-bottom p-2 gap-2'>
-      <NavLink to="">
-        <img src={alt} className='rounded-circle border border-2 border-dark' width={40}/>
-      </NavLink>
+      <Nav>
+        <Nav.Link href={`user/${userId}`}>
+        {tweet.image_user ? <img src={tweet.image_user} className='rounded-circle border border-2 border-dark' width={40}/> : <img src={alt} className='rounded-circle border border-2 border-dark' width={40}/>}
+        </Nav.Link>
+      </Nav>
       <div className='w-100'>
-        <NavLink style={{textDecoration :'none'}} to="/">
-          <div className='w-100 flex-column text-dark' >
+          <div className='w-100 flex-column' >
             <div className='d-flex justify-content-between'>
               <div className='fw-bold'>
-                {tweet.user.name}
+                <a className='text-dark' href='' style={{textDecoration :'none'}}>{tweet.user.name}</a>
               </div>
+              <Dropdown>
+                <Dropdown.Toggle bsPrefix as={"none"} >
+                  <NavLink className='text-dark'><HiDotsHorizontal/></NavLink>
+                </Dropdown.Toggle>
+                
+                <Dropdown.Menu>
+                  {tweet.user.id == userId ? <Dropdown.Item className='text-danger' onClick={ () => onClickDelete(tweet.tweet.id)}><FaRegTrashAlt />削除</Dropdown.Item> : null }
+                </Dropdown.Menu>
+              </Dropdown>
             </div>
-            <div className='w-100'>
-              {tweet.tweet.content}
-            </div>
-            {}
-            <div className='w-100'>
-              {tweet.tweet.image_url ? <img src={tweet.tweet.image_url} className='rounded-4 border border-1 w-100'></img> : null}
-            </div>
+            <NavLink style={{textDecoration :'none'}} to={`tweet/${tweet.tweet.id}`}>
+              <div className='w-100 text-dark'>
+                {tweet.tweet.content}
+              </div>
+              <div className='w-100'>
+                {tweet.tweet.image_url ? <img src={tweet.tweet.image_url} className='rounded-4 border border-1 w-100'></img> : null}
+              </div>
+            </NavLink>
           </div>
-        </NavLink>
-        
         <div className='mt-3'>
           <div className='d-flex justify-content-between mt-3'>
             <div>
